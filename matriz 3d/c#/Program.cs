7bb0a39758f5ecc6008c116c2394642e6b556bf7
Ana <gamboavegaanamariaa@gmail.com>
2026-09-10 20:18:48 -0600

    matriz 3d c#

---
+++ matriz 3d/c#/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,,] dim3 = {
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            },
+            {
+                {10, 11, 12},
+                {13, 14, 15},
+                {16, 17, 18}
+            }
+        };
+
+        Console.WriteLine("Los elementos del array son: ");
+        for (int i = 0; i < dim3.GetLength(0); i++)
+        {
+            for (int j = 0; j < dim3.GetLength(1); j++)
+            {
+                for (int k = 0; k < dim3.GetLength(2); k++)
+                {
+                    Console.Write(dim3[i, j, k] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
