adf85b2601f8db955e1f0a704245d71b65c1ef6a
Ana <gamboavegaanamariaa@gmail.com>
2026-09-10 18:44:37 -0600

    Reco por filas #

--- matri_reco_colum/obj/Debug/net10.0/matri_reco.AssemblyInfo.cs
+++ matri_reco_colum/obj/Debug/net10.0/matri_reco.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("matri_reco")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36b36e67417a1460ee52ee481960bdea3f362b00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0a7080bac2ad94d133cfd9b49c5948259ab72ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("matri_reco")]
 [assembly: System.Reflection.AssemblyTitleAttribute("matri_reco")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ matri_reco_fil/c#/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int r = 3;
+        int c = 3;
+        int[] arre = new int[r * c];
+        int[,] arre2 = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        int k = 0;
+
+        for (int x = 0; x < r; x++)
+        {
+            for (int y = 0; y < c; y++)
+            {
+                k = x * r + y;
+                arre[k] = arre2[x, y];
+                k = k + 1;
+            }
+        }
+
+        Console.WriteLine("Los elementos del array bidimensional son: ");
+        for (int x = 0; x < r; x++)
+        {
+            for (int y = 0; y < c; y++)
+            {
+                Console.Write(arre2[x, y] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("\nLos elementos del array unidimensional son: ");
+        for (int x = 0; x < r; x++)
+        {
+            for (int y = 0; y < c; y++)
+            {
+                Console.Write(arre[x * r + y] + " ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
