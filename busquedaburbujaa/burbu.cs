e1104f2bd704b9609af1b6cdbf36ce279b51d784
Ana <gamboavegaanamariaa@gmail.com>
2026-09-16 19:19:44 -0600

    busqueda burbuja c#

---
+++ busquedaburbujaa/burbu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Burbu(int[] a, int s)
+    {
+        for (int j = 0; j < s; j++)
+        {
+            bool cambio = false;
+            for (int i = 0; i < s - j - 1; i++)
+            {
+                if (a[i] > a[i + 1])
+                {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    cambio = true;
+                }
+            }
+            if (cambio == false)
+            {
+                break;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] a = { 15, 16, 11, 13, 14 };
+        int s = a.Length;
+
+        Console.WriteLine("Antes de ordenar los elementos del array son: ");
+        for (int i = 0; i < s; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+
+        Burbu(a, s);
+
+        Console.WriteLine("\nDespues de ordenar los elementos del array son: ");
+        for (int i = 0; i < s; i++)
+        {
+            Console.Write(a[i] + " ");
+        }
+    }
+}
