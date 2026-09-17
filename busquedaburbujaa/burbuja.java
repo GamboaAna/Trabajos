cad3b96c8bada47952cf498d1d5c5eb8f977fc4f
Ana <gamboavegaanamariaa@gmail.com>
2026-09-16 18:30:12 -0600

    busqueda burbuja Java

---
+++ busquedaburbujaa/burbuja.java
@@ -0,0 +1,36 @@
+public class burbuja {
+
+    public static void bubbleSort(int[] a) {
+        int s = a.length;
+        for (int i2 = 0; i2 < s; i2++) {
+            boolean cambio = false;
+            for (int i = 0; i < s - i2 - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    cambio = true;
+                }
+            }
+            if (cambio == false) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {15, 16, 11, 13, 14};
+
+        System.out.println("Antes de ordenar los elementos del array son: ");
+        for (int i : a) {
+            System.out.print(i + " ");
+        }
+
+        bubbleSort(a);
+
+        System.out.println("\nDespues de ordenar los elementos del array son: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.printf("%d ", a[i]);
+        }
+    }
+}
