f1912cf0462faf951d652ffcd74fce8ad88a4e1b
Ana <gamboavegaanamariaa@gmail.com>
2026-09-21 00:45:06 -0600

    Eliminacion al final py (no le habia cambiado nada jeje ups)

--- eliminacion/elifin/eli.py
+++ eliminacion/elifin/eli.py
@@ -1,10 +1,9 @@
 inputArr = [11, 21, 31, 41, 51, 61]
-position = 3
 print("Antes de la eliminacion, el array es: ")
 for j in range(len(inputArr)):
     print(inputArr[j], end=" ")
 
-del inputArr[position]
+inputArr.pop()
 
 print("\nDespues de la eliminacion, el array es: ")
 for j in range(len(inputArr)):
