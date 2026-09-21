f7d60cd78e7a94c8722fee4a4e22ea755f378545
Ana <gamboavegaanamariaa@gmail.com>
2026-09-21 01:09:04 -0600

    Eliminacion normal c++

---
+++ eliminacion/eliminacionnor/eli.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int inputArr[] = {11, 21, 31, 41, 51, 61};
+    int n = sizeof(inputArr) / sizeof(inputArr[0]);
+
+    cout << "Antes de la eliminacion, el array es: ";
+    for (int j = 0; j < n; j++) {
+        cout << inputArr[j] << " ";
+    }
+
+    for (int j = 0; j < n - 1; j++) {
+        inputArr[j] = inputArr[j + 1];
+    }
+    n--;
+
+    cout << "\nDespues de la eliminacion, el array es: ";
+    for (int j = 0; j < n; j++) {
+        cout << inputArr[j] << " ";
+    }
+
+    return 0;
+}
